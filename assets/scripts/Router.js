ddfac9fbf7dd6ca5b451672e5f4c152126c3154c
Elias <elias@Eliass-MacBook-Pro.local>
2021-11-07 00:43:31 -0700

    completed lab

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc
   }
 
   /**
@@ -65,5 +66,21 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+     if(!this[page]) {
+      console.log('error');
+      return
+    }
+    var hash;
+    if(page == 'home') {
+      hash = "";
+    }
+    else {
+      hash='#'+page
+    }
+    if(!statePopped && window.location.hash != hash) {
+      console.log("FALSE")
+      history.pushState(page, '', window.location + hash);
+    }
+    this[page]();
   }
 }
